232e9fc7c526c3019b23b315f4fd2236cd9c5431
Takumi Nakayama <takumi.n19971228@gmail.com>
2023-10-22 22:01:21 +0900

    タイピングの正誤判定が行える機能を記述しました

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 'use strict';
 
 {
+    const word = 'red';
+    let loc = 0; //typing時の何番目の文字かを判定する変数
+
+    const target = document.getElementById('target');
+    target.textContent = word;
+
     document.addEventListener('keydown', e => {
-        const target = document.getElementById('target');
-        target.textContent = e.key;
+        if (e.key === word[loc]) {
+            loc++;
+            target.textContent = '_'.repeat(loc) + word.substring(loc);
+        }
     });
 }
