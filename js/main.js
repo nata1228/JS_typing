19229bce1a9b378b4c58b6d6e02952185547eb72
Takumi Nakayama <takumi.n19971228@gmail.com>
2023-10-22 22:13:01 +0900

    wordが重複しないように編集しました

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 'use strict';
 
 {
-    const word = 'red';
+    function setWord() {
+        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+        target.textContent = word;
+        loc = 0;
+    }
+
+    const words = [
+        'red',
+        'blue',
+        'pink',
+    ];
+    let word;
     let loc = 0; //typing時の何番目の文字かを判定する変数
 
     const target = document.getElementById('target');
-    target.textContent = word;
+    setWord();
 
     document.addEventListener('keydown', e => {
-        if (e.key === word[loc]) {
-            loc++;
-            target.textContent = '_'.repeat(loc) + word.substring(loc);
+        if (e.key !== word[loc]) {
+            return;
+        }
+
+        loc++;
+        target.textContent = '_'.repeat(loc) + word.substring(loc);
+
+        if (loc === word.length) {
+            setWord();
         }
     });
 }
