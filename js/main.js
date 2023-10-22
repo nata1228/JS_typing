d04fd6ce4960b025ffa6a1c13ff8330f5d62a1f4
Takumi Nakayama <takumi.n19971228@gmail.com>
2023-10-22 22:24:49 +0900

    クリア時間が表示されるよう編集しました

--- js/main.js
+++ js/main.js
@@ -14,9 +14,14 @@
     ];
     let word;
     let loc = 0; //typing時の何番目の文字かを判定する変数
+    let startTime;
 
     const target = document.getElementById('target');
-    setWord();
+
+    document.addEventListener('click', () => {
+        startTime = Date.now();
+        setWord();
+    });
 
     document.addEventListener('keydown', e => {
         if (e.key !== word[loc]) {
@@ -27,6 +32,12 @@
         target.textContent = '_'.repeat(loc) + word.substring(loc);
 
         if (loc === word.length) {
+            if (words.length === 0) {
+                const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+                const result = document.getElementById('result');
+                result.textContent = `Finished! ${elapsedTime} seconds!`;
+                return;
+            }
             setWord();
         }
     });
